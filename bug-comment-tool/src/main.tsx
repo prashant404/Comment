fb3875d5a8d5e2f3f1e38c4caac0a081759a90a2
prashant404 <pkr214204@gmail.com>
2026-04-11 22:04:13 +0530

    analytics added

--- bug-comment-tool/src/main.tsx
+++ bug-comment-tool/src/main.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.scss";
+import { Analytics } from "@vercel/analytics/react";
 
-// 1. Import the vanilla JS injector, NOT the React component
-import { inject } from '@vercel/analytics';
-
-// 2. Fire the injector before the React app even renders
-inject();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Analytics/>
   </React.StrictMode>
 );
