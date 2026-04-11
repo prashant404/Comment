aaf49771b74fa5f9e5d236cc56eab3e72e3c65f2
prashant404 <pkr214204@gmail.com>
2026-04-11 21:42:26 +0530

    analytics added

--- bug-comment-tool/src/main.tsx
+++ bug-comment-tool/src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.scss";
+import { Analytics } from "@vercel/analytics/next"
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
+
   </React.StrictMode>
 );
+
